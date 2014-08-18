036b768b2cbaac09085325affc74bd05e53887fd
Andrey Kuzmin <unsoundscapes@gmail.com>
2014-08-18 23:01:38 +0400

    Fix formatting

--- test.js
+++ test.js
@@ -11,18 +11,18 @@ it('should transform css with multiple processors', function (cb) {
   , { map: false }  // omit source map for the test
   )
 
-	stream.on('data', function (file) {
+  stream.on('data', function (file) {
     var result = file.contents.toString('utf8')
     var target = 'a { color: black; color: black; color: black; color: black }'
-		assert.equal( result, target )
-		cb()
-	})
+    assert.equal( result, target )
+    cb()
+  })
 
-	stream.write(new gutil.File({
-		contents: new Buffer('a { color: black }')
-	}))
+  stream.write(new gutil.File({
+    contents: new Buffer('a { color: black }')
+  }))
 
-	stream.end()
+  stream.end()
 
 })
 
