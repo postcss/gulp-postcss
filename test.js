c3717c589ca4476c5c8bbffdf32aff78647a26e4
Daniel Husar <dano.husar@gmail.com>
2014-09-12 09:41:14 +0100

    add source maps test

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 
 var assert = require('assert')
 var gutil = require('gulp-util')
+var sourceMaps = require('gulp-sourcemaps')
 var postcss = require('./index')
 
 it('should transform css with multiple processors', function (cb) {
@@ -51,6 +52,35 @@ it ('should throw error if processors are not provided', function (cb) {
   assert.throws( function () { postcss('') }, gutil.PluginError )
   assert.throws( function () { postcss({}) }, gutil.PluginError )
   cb()
+})
+
+
+it ('should generate source maps', function (cb) {
+
+  var init = sourceMaps.init()
+  var write = sourceMaps.write()
+  var css = postcss(
+    [ doubler, doubler ]
+  )
+
+  init
+    .pipe(css)
+    .pipe(write)
+
+  write.on('data', function (file) {
+    assert.equal(file.sourceMap.mappings, 'AAAA,IAAI,cAAA,AAAa,cAAb,AAAa,cAAb,AAAa,aAAA,EAAC')
+    assert(/sourceMappingURL=data:application\/json;base64/.test(file.contents.toString()))
+    cb()
+  })
+
+  init.write(new gutil.File({
+    base: __dirname,
+    path: __dirname + '/fixture.css',
+    contents: new Buffer('a { color: black }')
+  }))
+
+  init.end()
+
 })
 
 
